15d7165407237159bfb842cb92bce04b4502fe01
Yuliyaa1404 <yuliyatv1404@gmail.com>
2022-08-11 14:06:06 +0400

    first commit

---
+++ traveland.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+	$(".destination__slider").owlCarousel({
+		items: 3,
+		//  center: true,
+		loop: true,
+		margin: 15,
+		nav: true,
+		dots: true,
+		lazyLoad: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		autoplayDelay: 1000,
+	});
+});
+
+$(document).ready(function () {
+	$(".feedback__row").owlCarousel({
+		items: 1,
+		//  center: true,
+		loop: true,
+		margin: 15,
+		nav: true,
+		dots: true,
+		lazyLoad: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		autoplayDelay: 1000,
+	});
+});
+
+$(document).ready(function () {
+	$(".blog__row").owlCarousel({
+		items: 2,
+		//  center: true,
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: true,
+		lazyLoad: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		autoplayDelay: 1000,
+	});
+});
